21c48ede68ee1e644e1935c0523cbda503279327
kcbhuwan <connect.bhuwan@gmail.com>
2019-11-07 18:38:47 +0545

    Added new tailwin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+    Dashboard Tailwind
+@endsection
+
+@section('content')
+    
+@endsection
+
+@section('scripts')
+    
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+<body class="bg-gray-100 h-screen antialiased leading-none">
+    <div id="app">
+        <nav class="bg-blue-900 shadow mb-8 py-6">
+            <div class="container mx-auto px-6 md:px-0">
+                <div class="flex items-center justify-center">
+                    <div class="mr-6">
+                        <a href="{{ url('/') }}" class="text-gray-100 text-base xl:text-xl font-medium uppercase">
+                            <i class="fas fa-columns"></i>
+                            Dashboard
+                        </a>
+                    </div>
+                    <div class="flex-1 text-right">
+                        @guest
+                            <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            @if (Route::has('register'))
+                                <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            @endif
+                        @else
+                            <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
+
+                            <a href="{{ route('logout') }}"
+                               class="no-underline hover:underline text-gray-300 text-sm p-3"
+                               onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                                {{ csrf_field() }}
+                            </form>
+                        @endguest
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
