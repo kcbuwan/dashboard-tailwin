31da642b5a43e00f2f7b687bdccc331188d2109a
kcbhuwan <connect.bhuwan@gmail.com>
2020-06-02 11:25:57 +0545

    Added user details

---
+++ resources/views/layouts/header-sticky.blade.php
@@ -0,0 +1,36 @@
+<div class="sticky top-0 z-40">
+    <div class="w-full h-16 px-10 bg-white shadow flex items-center justify-between">
+        <div class="flx">
+            <div class="inline-block lg:hidden flex items-center mr-4">
+                <button class="hover:text-blue-500 hover:border-white focus:outline-none navbar-burger">
+                    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" style="fill: black;">
+                        <title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg>
+                </button>
+            </div>
+            <nav aria-label="Breadcrumb" class="hidden-sm text-sm font-semibold">
+                <ol class="list-none p-0 inline-flex">
+                    <li class="flex items-center text-gray-500">
+                        <a href="#" class="text-gray-700">Home</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="fill-current w-3 h-3 mx-3 mt-1">
+                            <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
+                        </svg>
+                    </li>
+                    <li class="flex items-center">
+                        <a href="#" class="text-gray-600">Dashboard</a>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+        <div class="flex items-center relative">
+            <a href="#" class="p-3 items-center text-gray-600 hover:text-gray-700 font-semibold text-sm leading-none flex lg:inline-flex px-3">
+                <i class="fas fa-globe mr-2"></i>
+                <span class="text-sm font-semibold mr-2 text-left flex-auto">Website</span> 
+                <svg class="h-4 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg>
+            </a>
+            <a href="#" class="block text-sm font-medium text-gray-700 bg-gray-200 hover:bg-gray-400 rounded py-2 px-6 text-center">
+                Log out
+            </a>
+            
+        </div>
+    </div>
+</div>
