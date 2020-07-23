b4e464c58b69440a74e97b038cd75c0c70812ba6
kcbhuwan <connect.bhuwan@gmail.com>
2020-07-23 13:17:07 +0545

    Updated code indentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 // tailwind.config.js
 module.exports = {
-  theme: {
-    customForms: theme => ({
-      default: {
-        input: {
-          '&:focus': {
-            borderColor: theme('colors.teal.400'),
-            boxShadow: undefined,
-          }
-        },
-        select: {
-          '&:focus': {
-            borderColor: theme('colors.teal.400'),
-            boxShadow: undefined,
-          }
-        },
-        textarea: {
-          '&:focus': {
-            borderColor: theme('colors.teal.400'),
-            boxShadow: undefined,
-          }
-        },
-      },
-    })
-  },
-  plugins: [
-    require('@tailwindcss/custom-forms'),
-  ]
-}
+    theme: {
+        customForms: theme => ({
+            default: {
+                input: {
+                    "&:focus": {
+                        borderColor: theme("colors.teal.400"),
+                        boxShadow: undefined
+                    }
+                },
+                select: {
+                    "&:focus": {
+                        borderColor: theme("colors.teal.400"),
+                        boxShadow: undefined
+                    }
+                },
+                textarea: {
+                    "&:focus": {
+                        borderColor: theme("colors.teal.400"),
+                        boxShadow: undefined
+                    }
+                }
+            }
+        })
+    },
+    plugins: [require("@tailwindcss/custom-forms")]
+};
