97947f9491b0d58346cedf146f4dc2a104d815f9
aakgrg131 <aakgrg131@gmail.com>
2020-07-27 14:39:17 +0545

    updated form page

--- resources/views/admin/form-input.blade.php
+++ resources/views/admin/form-input.blade.php
@@ -12,36 +12,59 @@
             </div>
         
             <div class="lg:mt-0 flex flex-wrap">
-                <div class="w-full block rounded shadow bg-white mb-6 overflow-hidden">                
+                <div class="w-full block rounded shadow bg-white mb-6 px-6 py-4 overflow-hidden">                
                     <div class="flex flex-wrap mt-4 p-4">
                         <div class="w-full">
 
                             <div class="pb-4 pt-0 pl-0 pr-0">
 
+                                <div class="bg-teal-100 border border-teal-400 text-teal-700 mb-6 px-4 py-3 rounded relative" role="alert">
+                                    <strong class="font-bold">Success!</strong>
+                                    <span class="block sm:inline">Message sent successfully.</span>
+                                </div>
+
+                                <div class="bg-red-100 border border-red-400  mb-6 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                    <strong class="font-bold">Error!</strong>
+                                    <span class="block sm:inline">Message not sent.</span>
+                                </div>
+
+                                <div class="bg-blue-100 border border-blue-400 text-blue-700 mb-6 px-4 py-3 rounded relative" role="alert">
+                                    <strong class="font-bold">Info!</strong>
+                                    <span class="block sm:inline">This is a info alert—check it out! </span>
+                                </div>
+
+                                <div class="bg-orange-100 border border-orange-400 text-orange-700 mb-6 px-4 py-3 rounded relative" role="alert">
+                                    <strong class="font-bold">Warning!</strong>
+                                    <span class="block sm:inline">This is a warning alert—check it out!</span>
+                                </div>
+
+                                <hr class="w-full mb-6">
+
                                 <div class="flex flex-wrap">
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <label class="block text-gray-700 pb-2" for="product-name">
-                                            Inputs
+                                    <div class="w-full md:w-2/6 mb-6 pr-6">
+                                        <label class="block font-semibold text-gray-700" for="product-name">
+                                            Name
                                         </label>
+                                        <span class="text-gray-600 pb-2">This informations will be displayed publicly so be careful what you share.</span>
                                     </div>
 
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
+                                    <div class="w-full md:w-4/6 mb-6">
                                         <div class="w-full flex flex-wrap">
-                                            <input class="form-input mt-1 block w-full " placeholder="Name" id="name">
+                                            <input class="form-input mt-1 block w-full " placeholder="" id="name">
                                         </div>
                                     </div>
 
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <label class="block text-gray-700 pt-2" for="categories">
+                                    <div class="w-full md:w-2/6 mb-4">
+                                        <label class="block font-semibold text-gray-700 pt-2" for="categories">
                                             Add Select
                                         </label>
                                     </div>
 
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
+                                    <div class="w-full md:w-4/6 mb-6">
                                         <div class="flex flex-wrap">
                                             <div class="w-full md:w-5/6">
                                                 <select class="form-select mt-1 text-gray-500 block w-full">
-                                                    <option selected="">Select Category</option>
+                                                    <option selected=""></option>
                                                     <option>Car, Bikes &amp; Boats</option>
                                                 </select>
                                             </div>
@@ -56,16 +79,17 @@
                                     </div>
                                 </div>
 
+
                                 <div class="flex flex-wrap">
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <label class="block text-gray-700 pt-2" for="product-condition">
+                                    <div class="w-full md:w-2/6 mb-4">
+                                        <label class="block font-semibold text-gray-700 pt-2" for="product-condition">
                                             Select <span class="text-red-400">*</span>
                                         </label>
                                     </div>
 
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
+                                    <div class="w-full md:w-4/6 mb-4">
                                         <select class="form-select mt-1 text-gray-500 block w-full">
-                                            <option selected="">Select Some Option</option>
+                                            <option selected=""></option>
                                             <option>New</option>
                                             <option>Refurbished</option>
                                             <option>Used</option>
@@ -73,15 +97,18 @@
                                     </div>
                                 </div>
 
+                                <hr class="w-full mt-2 mb-6">
+
                                 <div class="flex flex-wrap">
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <span class="text-gray-700">Checkbox</span>
+                                    <div class="w-full md:w-2/6 mb-4">
+                                        <span class="text-gray-700 font-semibold">Checkbox</span>
                                     </div>
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
+
+                                    <div class="w-full md:w-4/6 mb-4">
                                         <div class="mt-0">
                                             <div>
                                                 <label class="inline-flex items-center">
-                                                    <input type="checkbox" class="form-checkbox text-teal-500 focus:shadow-none" checked>
+                                                    <input type="checkbox" class="form-checkbox text-teal-500 focus:shadow-none">
                                                     <span class="ml-2 text-md text-gray-600">Check 1</span>
                                                 </label>
                                             </div>
@@ -102,14 +129,15 @@
                                 </div>
 
                                 <div class="flex flex-wrap">
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <span class="text-gray-700">Radio Buttons</span>
+                                    <div class="w-full md:w-2/6 mb-4">
+                                        <span class="text-gray-700 font-semibold">Radio Buttons</span>
                                     </div>
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
+
+                                    <div class="w-full md:w-4/6 mb-4">
                                         <div class="mt-0">
                                             <div>
                                                 <label class="inline-flex items-center">
-                                                    <input type="radio" class="form-radio text-teal-500 focus:shadow-none" name="radio" value="1" checked>
+                                                    <input type="radio" class="form-radio text-teal-500 focus:shadow-none" name="radio" value="1">
                                                     <span class="ml-2 text-md text-gray-600">Option 1</span>
                                                 </label>
                                             </div>
@@ -129,26 +157,28 @@
                                     </div>
                                 </div>
 
+                                <hr class="w-full mt-2 mb-6">
+
                                 <div class="flex flex-wrap">
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <label class="block text-gray-700 pt-2" for="meta-title">
+                                    <div class="w-full md:w-2/6 mb-4">
+                                        <label class="block text-gray-700 font-semibold pt-2" for="meta-title">
                                             Textarea
                                         </label>
                                     </div>
 
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
-                                        <textarea class="form-textarea mt-1 block w-full" rows="3" placeholder="Brief description here.." id="comment"></textarea>
+                                    <div class="w-full md:w-4/6 mb-4">
+                                        <textarea class="form-textarea mt-1 block w-full" rows="3" placeholder="" id="comment"></textarea>
                                     </div>
                                 </div>
 
                                 <div class="flex flex-wrap">
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <label class="block text-gray-700 pt-2" for="meta-title">
+                                    <div class="w-full md:w-2/6 mt-2 mb-4">
+                                        <label class="block text-gray-700 font-semibold pt-2" for="meta-title">
                                             Upload
                                         </label>
                                     </div>
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
-                                        <label class="block text-center items-center px-4 py-4 bg-white text-gray-500 rounded-sm border-dashed border-2 border-gray-400 cursor-pointer hover:text-blue-600">
+                                    <div class="w-full md:w-4/6 mt-2 mb-4">
+                                        <label class="block text-center items-center px-4 py-6 bg-white text-gray-500 rounded-sm border-dashed border-2 border-gray-400 cursor-pointer hover:text-blue-600">
                                             <div class="inline-flex text-center items-center">
                                                 <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                                     <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
@@ -162,15 +192,17 @@
                                     </div>
                                 </div>
 
+                                <hr class="w-full mt-2 mb-4">
+
                                 <div class="flex flex-wrap">
-                                    <div class="w-full md:w-1/4 px-4 mb-4">
-                                        <label class="block text-gray-700 pt-2" for="meta-title">
+                                    <div class="w-full md:w-2/6 mb-4">
+                                        <label class="block font-semibold text-gray-700 pt-2" for="meta-title">
                                             Button
                                         </label>
                                     </div>
-                                    <div class="w-full md:w-3/4 px-4 mb-4">
+                                    <div class="w-full md:w-4/6 mb-4">
                                         <div class="inline-block">
-                                            <button class="bg-blue-400 hover:bg-blue-600 outline-none focus:border-blue-900 rounded text-white py-2 pb-3 px-6 inline-flex font-semibold">
+                                            <button class="bg-teal-600 hover:bg-teal-700 outline-none focus:border-blue-900 rounded text-white py-2 pb-3 px-6 inline-flex font-semibold">
                                                 Submit
                                             </button>
                                         </div>
