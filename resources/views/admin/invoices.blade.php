bee1c42da53c5779ee1100159853a7508f3a91cf
kcbhuwan <connect.bhuwan@gmail.com>
2020-08-09 15:51:30 +0545

    Fixed search field

--- resources/views/admin/invoices.blade.php
+++ resources/views/admin/invoices.blade.php
@@ -14,7 +14,7 @@
                             <div class="ds-dataset-1"></div>
                         </span>
                     </span>
-                    <div class="pointer-events-none absolute inset-y-0 right-0 pl-4 pr-4 flex items-left">
+                    <div class="pointer-events-none absolute inset-y-0 right-0 pl-4 pr-4 flex items-center">
                         <svg class="fill-current pointer-events-none text-gray-600 w-4 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z">
                             </path>
